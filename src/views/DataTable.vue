15d15cbd97bf225c3bb39af09c062b9844c69dca
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-30 18:32:49 +0100

    add view for data table

---
+++ src/views/DataTable.vue
@@ -0,0 +1,23 @@
+<template>
+  <TableData />
+</template>
+
+<script>
+import TableData from '../components/tableData/TableData.vue';
+
+export default {
+  name: 'DataTable',
+  data() {
+    return {
+      columns: [
+        { name: 'name', text: 'Name'},
+        { name: 'isbn', text: 'ISBN'},
+        { name: 'authors', text: 'Authors'},
+        { name: 'number_of_pages', text: 'Nummber of Pages'},
+        { name: 'country', text: 'Country'},
+        { name: 'released', text: 'Released'},
+      ]
+    }
+  }
+}
+</script>
