bd6543d0b64115cf82a073f064dd40a2a20d9f69
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-30 18:32:18 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+  <TableData />
+</template>
+
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue';
+import TableData from './components/tableData/TableData.vue';
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  border: 2px solid gold;
+}
+</style>
