37b87d0d5b1ed01981ff8a68ce9078917e5fc2df
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-30 19:44:55 +0100

    remname component

--- src/components/table-base/TableBase.vue
+++ src/components/table-base/TableBase.vue
@@ -2,24 +2,24 @@
   <table class="table">
     <thead>
       <tr>
-        <th>
-          <div v-for="th in tableHeader" :key="th" class=head-wrap>
-            <span>{{ th }}</span>
+        <th v-for="th in tableHeader" :key="th">
+          <div class=head-wrap>
+            <span>{{ th.text }}</span>
             <span>
-              <svg></svg>
-
-
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+              </svg>
             </span>
           </div>
         </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="td in tableHeader" :key="td" class=head-wrap>
+      <tr v-for="td in TableBase" :key="td" class=head-wrap>
         <td>{{ td.name }}</td>
         <td>{{ td.isbn }}</td>
         <td>{{ td.authors }}</td>
-        <td>{{ td.number_of_pages }}</td>
+        <td>{{ td.numberOfPages }}</td>
         <td>{{ td.country }}</td>
         <!-- yyyy - mm - dd -->
         <td>{{ td.released }}</td> 
@@ -30,10 +30,10 @@
 
 <script>
 
-import './TableData.css';
+import './TableBase.css';
 
 export default {
-  name: 'TableData',
+  name: 'TableBase',
   props: {
     columns: Array,
     entries: Array
@@ -42,7 +42,7 @@ export default {
     tableHeader() {
       return this.columns || []
     },
-    tableData() {
+    TableBase() {
       return this.entries || null
     }
   }
