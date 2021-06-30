f61dba3bb1fc17c21823b180753fdfeb82c9429c
Damilola Faseun <damilola.faseun@gmail.com>
2021-06-30 22:37:31 +0100

    Add filter icon to table base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
-  <TableData />
+  <div class="container">
+    <DataTable />
+  </div>
 </template>
 
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue';
-import TableData from './components/tableData/TableData.vue';
+import DataTable from './views/DataTable.vue';
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
@@ -14,6 +15,9 @@ import TableData from './components/tableData/TableData.vue';
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  border: 2px solid gold;
+}
+
+.container {
+  margin: 5px;
 }
 </style>
--- src/components/table-base/TableBase.vue
+++ src/components/table-base/TableBase.vue
@@ -3,12 +3,14 @@
     <thead>
       <tr>
         <th v-for="th in tableHeader" :key="th">
-          <div class=head-wrap>
+          <div class="header-wrap">
             <span>{{ th.text }}</span>
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            <span style="display:flex, justify-content: center, align-items: center">
+              <svg style="color: #DCDCDC" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-              </svg>
+            </svg>
+
+
             </span>
           </div>
         </th>
