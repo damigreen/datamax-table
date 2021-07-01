bbdcb437e0702256f2438fca223c2c2d1776b618
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-01 14:39:51 +0100

    Add pagination to table

--- src/components/table-base/TableBase.vue
+++ src/components/table-base/TableBase.vue
@@ -1,16 +1,14 @@
-<template>
+<template #data="{pageNumber}">
   <table class="table">
     <thead>
       <tr>
-        <th v-for="th in tableHeader" :key="th">
+        <th v-for="(th) in tableHeader" :key="th">
           <div class="header-wrap">
             <span>{{ th.text }}</span>
             <span style="display:flex, justify-content: center, align-items: center">
               <svg style="color: #DCDCDC" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
             </svg>
-
-
             </span>
           </div>
         </th>
@@ -28,6 +26,23 @@
       </tr>
     </tbody>
   </table>  
+
+  <div class='pagination-wrap'>
+      <!-- <span> -->
+      <!-- 
+            (-) [1] [2] [3] [4] [5] (+)    - [6] [7] [8] +
+      -->
+
+    <!-- <button v-if='pageNumber <= Math.ceil(this.entries.length / this.currentEntry)' @click='pageNumber = pageNumber - 1' class='paginate-button'>PREV</button> -->
+    <button @click='pageNumber = pageNumber === 1 ? 1 : pageNumber - 1' class='paginate-button'>PREV</button>
+    <span class="paginate-number" v-for="(item, i) in numberOfPages" :key="item">
+        <button >{{ i + 1 }} [{{pageNumber}}]</button>
+    </span>
+    <button @click='pageNumber = pageNumber + 1' class='paginate-button' style="margin-left: 4px">NEXT</button>
+
+    <slot name='data' :pageNumber='pageNumber' :numberOfPages='numberOfPages' />
+  </div>
+  <span style='background: red; padding: 5px'>{{this.pageNumber}}</span>
 </template>
 
 <script>
@@ -38,16 +53,30 @@ export default {
   name: 'TableBase',
   props: {
     columns: Array,
-    entries: Array
+    entries: Array,
+    pageNumber: Number,
+    currentEntry: Number,
+    numberOfPages: Number,
   },
   computed: {
     tableHeader() {
-      return this.columns || []
+      return this.columns || [];
     },
     TableBase() {
-      return this.entries || null
+      return this.entries || null;
+    },
+  },
+  methods: {
+    prevPage () {
+        console.log(this.pageNumber);
+      
+      this.pageNumber = this.pageNumber - 1;
+    },
+    nextPage () {
+      console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
+      this.pageNumber = this.pageNumber + 1;
     }
-  }
+  },
 }
 
 </script>
--- src/views/DataTable.vue
+++ src/views/DataTable.vue
@@ -1,8 +1,9 @@
-<template>
+<template #data='pageNumber'>
   <div class="table-wrap">
     <div class="flex-row">Show
       <span class="mg-r">
         <!-- <select v-model="currentEntry" @change="paginateEntries" class="select"> -->
+
         <select v-model="currentEntry" @change="filEnt" class="select">
           <option v-for="se in showEntries" :key="se" :value="se" >{{ se }}</option>
         </select>
@@ -10,7 +11,16 @@
     </div>
 
     <!-- <TableBase :columns="columns" :entries="entries"  /> -->
-    <TableBase :columns="columns" :entries="filteredEntries"  />
+    <!-- <TableBase  :columns="columns" :entries="filteredEntries" :pageNumber="paginateTable" :nextPage="nextPage" :prevPage="prevPage"  /> -->
+    <TableBase
+      :columns="columns"
+      :entries="filteredEntries"
+      :pageNumber="paginateTable"
+      :numberOfPages="pageToDisplay"
+    />
+    <div class='show-info'>
+      <div>Show {{ 4 }} to {{6}} of {{ this.currentEntry }}</div>
+    </div>
   </div>
 
 </template>
@@ -33,9 +43,11 @@ export default {
       ],
 
       entries: [],
-      showEntries: [5, 10, 12],
-      currentEntry: 5,
-      filteredEntries: []
+      showEntries: [5, 8, 10, 12],
+      currentEntry: 8,
+      filteredEntries: [],
+      pageNumber: 1,
+      numberOfPages: 1
     }
   },
   components: {
@@ -47,14 +59,34 @@ export default {
     });
 
     this.paginateEntries().then(res => {
+      console.log(res)
       this.filteredEntries = res;
     });
   },
+  /* 
+                    [1]                          [2]
+          (-) [1] [2] [3] [4] [5] (+)    <-- [6] [7] [8] -->
+  */
   computed: {
     filEnt() {
+      this.numbering = 0;
       this.paginateEntries().then(res => {
+        for (var i = 0; i < res.length; i++) {
+          this.numbering += 1;
+          // console.log(this.numbering);
+        }
         this.filteredEntries = res;
-      })
+      });
+    },
+    showInfo() {
+      console.log('show info')
+    },
+    paginateTable () {
+      return Math.ceil(this.entries.length / this.currentEntry);
+    },
+    pageToDisplay () {
+      console.log(Math.ceil(this.entries.length / 8));
+      return Math.ceil(this.entries.length / 8);
     }
   },
   methods: {
@@ -65,6 +97,15 @@ export default {
     async paginateEntries() {
       const response = await fetch(`https://www.anapioficeandfire.com/api/books?page=1&pageSize=${this.currentEntry}`);
       return response.json();
+    },
+    nextPage () {
+      console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
+      this.pageNumber++;
+    },
+    prevPage () {
+      console.log(this.pageNumber--);
+      
+      this.pageNumber--;
     }
   }
 }
