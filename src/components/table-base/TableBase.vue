cb3a3f942dc637c1f5aa3bc04c19bb0822e6341f
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-02 09:59:38 +0100

    Format date and authors

--- src/components/table-base/TableBase.vue
+++ src/components/table-base/TableBase.vue
@@ -5,25 +5,25 @@
         <th v-for="(th) in tableHeader" :key="th">
           <div class="header-wrap">
             <span>{{ th.text }}</span>
-            <span style="display:flex, justify-content: center, align-items: center">
-              <svg style="color: #DCDCDC" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-            </svg>
-            </span>
           </div>
         </th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="td in TableBase" :key="td" class=head-wrap>
-      <!-- <tr v-for="td in paginatedData" :key="td" class=head-wrap> -->
         <td>{{ td.name }}</td>
         <td>{{ td.isbn }}</td>
-        <td v-for="au in td.authors" :key="au">{{ au }}</td>
+        <td >
+          <div v-for="au in td.authors" :key="au">{{ au }}</div>
+        </td>
         <td>{{ td.numberOfPages }}</td>
         <td>{{ td.country }}</td>
         <!-- yyyy - mm - dd -->
-        <td>{{ td.released }}</td> 
+        <td>
+          {{
+            td.released.slice(0, 10)
+          }}
+        </td> 
       </tr>
     </tbody>
   </table>  
@@ -50,26 +50,7 @@ export default {
     TableBase() {
       return this.entries || null;
     },
-    // paginatedData () {
-    //   const start = (this.pageNumber * 8) - (8 - 1);
-    //   const end = start + (8 - 1);
-    //   console.log(this.pageNumber)
-    //   console.log('---------------------')
-    //   console.log(start);
-    //   console.log(end)
-    //   this.pageNumber = this.pageNumber + 1;
-    //   this.entries = this.entries.slice(start, end)
-    // },
   },
-  // methods: {
-  //   prev () {
-  //     console.log('--------------------------------------', this.pageNumber)
-  //   },
-  //   nextPage () {
-  //     console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh');
-  //     this.pageNumber = this.pageNumber + 1;
-  //   }
-  // },
 }
 
 </script>
